c349ab52cd4441c0e67943b8791f9068be82b982
Enigmo96 <enigmo96@outlook.com>
2021-02-03 15:43:02 +0100

    feat: added MainViewModel with Koin lib

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainActivity.kt
@@ -3,8 +3,12 @@ package com.ivanmorgillo.corsoandroid.teama
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,7 +16,9 @@ class MainActivity : AppCompatActivity() {
         // dobbiamo creare un binding alla UI
         val adapter = RecipesAdapter()
         recipe_list.adapter = adapter
-        adapter.setRecipes(recipes)
+
+        val recipesList = viewModel.getRecipes()
+        adapter.setRecipes(recipesList)
 
     }
 }
@@ -20,19 +26,4 @@ class MainActivity : AppCompatActivity() {
 data class RecipeUI(
     val title: String,
     val image: String
-)
-
-val recipes = listOf<RecipeUI>(
-    RecipeUI(title = "Pizza1", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza2", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza3", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza4", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza5", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza6", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza7", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza8", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza9", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza10", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza11", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
-    RecipeUI(title = "Pizza12", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
 )
