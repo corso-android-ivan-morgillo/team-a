ec642ea8ddab6b4534e330cfbf00162d8498d3bf
Enigmo96 <enigmo96@outlook.com>
2021-02-03 12:00:13 +0100

    feat: improved CardView layout fix: Recipes strings changed: Renamed app to "Delicious"

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainActivity.kt
@@ -15,37 +15,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
         // dobbiamo creare un binding alla UI
-        val adapter : RecipiesAdapter = RecipiesAdapter()
+        val adapter : RecipesAdapter = RecipesAdapter()
         recipe_list.adapter = adapter
-        adapter.setRecipies(recipies)
+        adapter.setRecipes(recipes)
 
     }
 }
 
-class RecipiesAdapter : RecyclerView.Adapter<RecipeViewHolder>() {
-    private var recipies: List<RecipeUI> = emptyList()
+class RecipesAdapter : RecyclerView.Adapter<RecipeViewHolder>() {
+    private var recipes: List<RecipeUI> = emptyList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_item, parent, false)
         return RecipeViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-        holder.bind(recipies[position])
+        holder.bind(recipes[position])
     }
 
     override fun getItemCount(): Int {
-        return recipies.size
+        return recipes.size
     }
-    fun setRecipies(items : List<RecipeUI>){
-        recipies = items
+    
+    fun setRecipes(items : List<RecipeUI>){
+        recipes = items
         notifyDataSetChanged()
     }
 }
 
 class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val title = itemView.findViewById<TextView>(R.id.recipe_title)
+    val title = itemView.findViewById<TextView>(R.id.recipe_title)
     val image = itemView.findViewById<ImageView>(R.id.recipe_image)
+
     fun bind(item: RecipeUI) {
         title.text = item.title
         image.load(item.image)
@@ -57,7 +61,7 @@ data class RecipeUI(
     val image: String
 )
 
-val recipies = listOf<RecipeUI>(
+val recipes = listOf<RecipeUI>(
     RecipeUI(title = "Spaghetti1", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
     RecipeUI(title = "Spaghetti2", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
     RecipeUI(title = "Spaghetti3", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
