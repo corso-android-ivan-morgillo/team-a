715f82497c4ad72fceffbc55ba186dccde6ed23b
Enigmo96 <enigmo96@outlook.com>
2021-02-03 14:28:48 +0100

    refactor: extracted RecipesAdapter to new file

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainActivity.kt
@@ -2,13 +2,6 @@ package com.ivanmorgillo.corsoandroid.teama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,42 +10,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         
         // dobbiamo creare un binding alla UI
-        val adapter : RecipesAdapter = RecipesAdapter()
+        val adapter = RecipesAdapter()
         recipe_list.adapter = adapter
         adapter.setRecipes(recipes)
 
-    }
-}
-
-class RecipesAdapter : RecyclerView.Adapter<RecipeViewHolder>() {
-    private var recipes: List<RecipeUI> = emptyList()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_item, parent, false)
-        return RecipeViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
-        holder.bind(recipes[position])
-    }
-
-    override fun getItemCount(): Int {
-        return recipes.size
-    }
-    
-    fun setRecipes(items : List<RecipeUI>){
-        recipes = items
-        notifyDataSetChanged()
-    }
-}
-
-class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val title = itemView.findViewById<TextView>(R.id.recipe_title)
-    val image = itemView.findViewById<ImageView>(R.id.recipe_image)
-
-    fun bind(item: RecipeUI) {
-        title.text = item.title
-        image.load(item.image)
     }
 }
 
@@ -62,16 +23,16 @@ data class RecipeUI(
 )
 
 val recipes = listOf<RecipeUI>(
-    RecipeUI(title = "Spaghetti1", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti2", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti3", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti4", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti5", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti6", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti7", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti8", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti9", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti10", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti11", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
-    RecipeUI(title = "Spaghetti12", image = "https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg"),
+    RecipeUI(title = "Pizza1", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza2", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza3", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza4", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza5", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza6", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza7", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza8", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza9", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza10", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza11", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
+    RecipeUI(title = "Pizza12", image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"),
 )
---
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/RecipesAdapter.kt
@@ -0,0 +1,41 @@
+package com.ivanmorgillo.corsoandroid.teama
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+
+class RecipesAdapter : RecyclerView.Adapter<RecipeViewHolder>() {
+    private var recipes = emptyList<RecipeUI>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_item, parent, false)
+        return RecipeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        holder.bind(recipes[position])
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+    fun setRecipes(items : List<RecipeUI>){
+        recipes = items
+        notifyDataSetChanged()
+    }
+}
+
+class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val title = itemView.findViewById<TextView>(R.id.recipe_title)
+    val image = itemView.findViewById<ImageView>(R.id.recipe_image)
+
+    fun bind(item: RecipeUI) {
+        title.text = item.title
+        image.load(item.image)
+    }
+}
