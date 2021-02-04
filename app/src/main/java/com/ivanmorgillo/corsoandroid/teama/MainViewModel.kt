c66556451cab3d0964faff0121d6d94c3a693ba9
Gaetano Cimino <gaetano.cimino17@gmail.com>
2021-02-04 14:09:43 +0100

    chore: syntax fix to MainViewModel.kt

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainViewModel.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainViewModel.kt
@@ -1,15 +1,35 @@
 package com.ivanmorgillo.corsoandroid.teama
 
 import androidx.lifecycle.ViewModel
-
+private const val MAXRANGE = 10
 class MainViewModel : ViewModel() {
 
     fun getRecipes() = recipes
 
     private val title = "Pizza1"
     private val image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"
-    private val maxRange = 10
-    private val recipes = (1.rangeTo(maxRange)).map {
+    private val recipes = (1..MAXRANGE).map {
         RecipeUI(title = title, image = image)
     }
+    /** private val recipes = listOf(
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image),
+    RecipeUI(title = title, image = image)
+    )
+     *
+     * Metodo alternativo per fare la lista :
+     *  private val maxRange = 10
+    private val recipes = (1.rangeTo(maxRange)).map {
+    RecipeUI(title = title, image = image)
+    }
+     * */
 }
