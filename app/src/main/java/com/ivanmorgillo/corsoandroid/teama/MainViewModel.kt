5bddf6699ec0be145cf4b2a3b27912347bd5d746
Gaetano Cimino <gaetano.cimino17@gmail.com>
2021-02-04 13:08:06 +0100

    refactor:  added functionality to MainViewModel.kt

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainViewModel.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MainViewModel.kt
@@ -8,18 +8,7 @@ class MainViewModel : ViewModel() {
 
     private val title = "Pizza1"
     private val image = "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg"
-    private val recipes = listOf(
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image),
-            RecipeUI(title = title, image = image)
-    )
+    private val recipes = (1.rangeTo(10)).map {
+        RecipeUI(title = title, image = image)
+    }
 }
