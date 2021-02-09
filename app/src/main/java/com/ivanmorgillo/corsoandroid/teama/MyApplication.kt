f5a44e1ac64b6234ef1ff634ccc6fc9be1d2edad
Enigmo96 <enigmo96@outlook.com>
2021-02-09 14:25:38 +0100

    feat: added Timber library

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MyApplication.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/MyApplication.kt
@@ -5,6 +5,9 @@ import android.os.StrictMode
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
+
+import timber.log.Timber.DebugTree
 
 @Suppress("unused")
 class MyApplication : Application() {
@@ -16,6 +19,11 @@ class MyApplication : Application() {
             androidLogger()
             androidContext(this@MyApplication)
             modules(appModule)
+        }
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            //Timber.plant(CrashReportingTree())
         }
     }
 
