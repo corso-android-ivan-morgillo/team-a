728abff99f52074e4b88f7d991588aa8dc01a88c
Enigmo96 <enigmo96@outlook.com>
2021-02-22 09:20:16 +0100

    feat: added base files for Category Screen

---
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/category/CategoryFragment.kt
@@ -0,0 +1,16 @@
+package com.ivanmorgillo.corsoandroid.teama.category
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ivanmorgillo.corsoandroid.teama.R
+
+class CategoryFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+}
