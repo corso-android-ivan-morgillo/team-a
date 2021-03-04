ed9a0fba1aa35e959b8a0d51c3768a8f2d0f3411
Enigmo96 <enigmo96@outlook.com>
2021-03-04 13:04:25 +0100

    fix: cleaned cache after load data error closes: #113

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/category/CategoryRepository.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/category/CategoryRepository.kt
@@ -12,8 +12,13 @@ class CategoryRepositoryImpl(private val api: NetworkAPI) : CategoryRepository {
     override suspend fun loadCategories(forced: Boolean): LoadCategoryResult {
         return if (cache == null || forced) {
             val result = api.loadCategories()
-            cache = result
-            result
+            if (result is LoadCategoryResult.Success) { // se il nuovo caricamento è avvenuto con successo
+                cache = result // aggiorna la cache
+                result // e ritorna il nuovo risultato
+            } else {
+                cache = null // necessario affinchè la cache venga azzerata nel caso in cui ricarico senza internet
+                result // e ritorna l'errore
+            }
         } else {
             cache!!
         }
