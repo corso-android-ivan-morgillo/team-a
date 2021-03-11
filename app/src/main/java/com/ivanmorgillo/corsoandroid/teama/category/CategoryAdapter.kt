92691ac66ffff0dd38ae6e9fbffcb4d82c253631
Marco Trusolino <trusolino.marco@educ.di.unito.it>
2021-03-11 11:56:46 +0100

    fix: category info showed expanded multiple times closes: #154

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/category/CategoryAdapter.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/category/CategoryAdapter.kt
@@ -22,6 +22,7 @@ class CategoryAdapter(private val onclick: (CategoryUI, View) -> Unit) : Recycle
 
     override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
         holder.bind(categories[position], onclick)
+        holder.setIsRecyclable(false)
     }
 
     override fun getItemCount(): Int {
