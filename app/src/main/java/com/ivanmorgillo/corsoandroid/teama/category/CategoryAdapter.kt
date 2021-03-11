88f1961d89d2cc8d9ae782f60a544f6757908528
Guaik4 <torres.carolina19@gmail.com>
2021-03-11 09:56:18 +0100

    feat: new Category UI.

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/category/CategoryAdapter.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/category/CategoryAdapter.kt
@@ -41,9 +41,9 @@ class CategoryAdapter(private val onclick: (CategoryUI, View) -> Unit) : Recycle
 class CategoryViewHolder(private val binding: CategoryItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: CategoryUI, onclick: (CategoryUI, View) -> Unit) {
-        val categoryFlagAdapter = CategoryFlagAdapter()
+        /*val categoryFlagAdapter = CategoryFlagAdapter()
         binding.flagList.adapter = categoryFlagAdapter
-        categoryFlagAdapter.setFlagCategories(item.flags)
+        categoryFlagAdapter.setFlagCategories(item.flags)*/
         val recipesCounterText = item.recipesCount + " " + binding.root.resources.getString(R.string.recipes)
         binding.recipeCounter.text = recipesCounterText
         binding.categoryTitle.text = item.title
