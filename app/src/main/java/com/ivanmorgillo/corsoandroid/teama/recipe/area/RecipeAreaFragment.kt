095a238b6912e88dd394859d7fe8a3733927f8a3
Guaik4 <torres.carolina19@gmail.com>
2021-03-24 13:00:58 +0100

    chore: work in progress, Areafragment anda AreaViewModel

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/recipe/area/RecipeAreaFragment.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/recipe/area/RecipeAreaFragment.kt
@@ -5,25 +5,58 @@ import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.transition.MaterialElevationScale
 import com.ivanmorgillo.corsoandroid.teama.R
 import com.ivanmorgillo.corsoandroid.teama.databinding.FragmentRecipeAreaBinding
-import com.ivanmorgillo.corsoandroid.teama.extension.exhaustive
-import com.ivanmorgillo.corsoandroid.teama.recipe.*
 import com.ivanmorgillo.corsoandroid.teama.utils.viewBinding
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import timber.log.Timber
 
-class RecipeAreaFragment : Fragment(R.layout.fragment_recipe_area) {
+class RecipeAreaFragment : Fragment(R.layout.fragment_recipe_area), SearchView.OnQueryTextListener {
+    private val viewModel: RecipeAreaViewModel by viewModel()
+    private val binding by viewBinding(FragmentRecipeAreaBinding::bind)
+    private val args: RecipeAreaFragmentArgs by navArgs()
+    private var lastClickedItem: View? = null
+    private var areaName = ""
 
-    // Equivalente alla onCreate di un activity
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        postponeEnterTransition()
+        view.doOnPreDraw { startPostponedEnterTransition() }
+        binding.recipesAreaRefresh.setOnRefreshListener {
+            viewModel.send(RecipeAreaScreenEvent.OnReady(areaName))
+        }
+        val adapter = RecipesAreaAdapter { item, view ->
+            lastClickedItem = view
+            exitTransition = MaterialElevationScale(false).apply {
+                duration = resources.getInteger(R.integer.motion_duration_large).toLong()
+            }
+            reenterTransition = MaterialElevationScale(true).apply {
+                duration = resources.getInteger(R.integer.motion_duration_large).toLong()
+            }
+            viewModel.send(OnRecipeAreaClick(item))
+        }
+        binding.recipeAreaList.adapter = adapter
+        areaName = args.areaName
+        if (areaName.isEmpty()) {
+            findNavController().popBackStack()
+        } else {
 
+            viewModel.states.observe(viewLifecycleOwner, { stare ->
+                when
+            }
+            )
+        }
+        
 
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/recipe/area/RecipeAreaViewModel.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/recipe/area/RecipeAreaViewModel.kt
@@ -1,7 +1,67 @@
 package com.ivanmorgillo.corsoandroid.teama.recipe.area
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ateam.delicious.domain.repository.RecipesRepository
+import com.ateam.delicious.domain.result.LoadRecipeResult
+import com.ivanmorgillo.corsoandroid.teama.Screens
 import com.ivanmorgillo.corsoandroid.teama.Tracking
+import com.ivanmorgillo.corsoandroid.teama.crashlytics.SingleLiveEvent
+import com.ivanmorgillo.corsoandroid.teama.extension.exhaustive
+import com.ivanmorgillo.corsoandroid.teama.recipe.RecipeUI
+import kotlinx.coroutines.launch
 
-class RecipeAreaViewModel(private val repository: RecipesRepository, private val tracking: Tracking) {
+class RecipeAreaViewModel(private val repository: RecipesRepository, private val tracking: Tracking) : ViewModel() {
+    private var areaRecipes: List<RecipeUI>? = null
+    val states = MutableLiveData<RecipeAreaScreenStates>()
+    val actions = SingleLiveEvent<RecipeAreaScreenAction>()
+
+    init {
+        tracking.logScreen(Screens.AreaRecipes)
+    }
+
+    fun send(event: RecipeAreaScreenEvent) {
+        when (event) {
+            is RecipeAreaScreenEvent.OnReady -> loadContent(event.areaName)
+            is RecipeAreaScreenEvent.OnRecipeAreaClick -> onRecipeAreaClick(event)
+            is RecipeAreaScreenEvent.OnRecipeAreaSearch -> onRecipeAreaSearch(event.query)
+            is RecipeAreaScreenEvent.OnRefresh -> loadContent(event.areaName)
+        }.exhaustive
+    }
+
+    private fun loadContent(areaName: String) {
+        states.postValue(RecipeAreaScreenStates.Loading)
+        viewModelScope.launch {
+            val result = repository.loadRecipesByArea(areaName)
+            when (result) {
+                is LoadRecipeResult.Failure -> onFailure(result)
+                is LoadRecipeResult.Success -> onSuccess(result)
+            }.exhaustive
+        }
+    }
+}
+
+sealed class RecipeAreaScreenAction {
+    data class NavigateToDetail(val areaRecipe: RecipeUI) : RecipeAreaScreenAction()
+    object ShowNoInternetMessage : RecipeAreaScreenAction()
+    object ShowSlowInternetMessage : RecipeAreaScreenAction()
+    object ShowServerErrorMessage : RecipeAreaScreenAction()
+    object ShowInterruptedRequestMessage : RecipeAreaScreenAction()
+    object ShowNoRecipeFoundMessage : RecipeAreaScreenAction()
+}
+
+sealed class RecipeAreaScreenStates {
+
+    object Loading : RecipeAreaScreenStates()
+    object Error : RecipeAreaScreenStates()
+    data class Content(val areaRecipes: List<RecipeUI>) : RecipeAreaScreenStates()
+}
+
+sealed class RecipeAreaScreenEvent {
+
+    data class OnRecipeAreaClick(val areaRecipe: RecipeUI) : RecipeAreaScreenEvent()
+    data class OnReady(val areaName: String) : RecipeAreaScreenEvent()
+    data class OnRefresh(val areaName: String) : RecipeAreaScreenEvent()
+    data class OnRecipeAreaSearch(val query: String) : RecipeAreaScreenEvent()
 }
