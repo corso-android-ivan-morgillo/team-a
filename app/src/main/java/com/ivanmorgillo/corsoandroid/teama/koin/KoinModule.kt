65979c1bc12bd8587897413d4a718708fe1331d9
Gaetano Cimino <gaetano.cimino17@gmail.com>
2021-03-29 11:51:51 +0200

    refactor: bug fixes for Ci Build.

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teama/koin/KoinModule.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teama/koin/KoinModule.kt
@@ -27,7 +27,6 @@ import com.ivanmorgillo.corsoandroid.teama.favourite.FavouriteViewModel
 import com.ivanmorgillo.corsoandroid.teama.home.HomeViewModel
 import com.ivanmorgillo.corsoandroid.teama.home.IngredientViewModel
 import com.ivanmorgillo.corsoandroid.teama.recipe.RecipeViewModel
-import com.ivanmorgillo.corsoandroid.teama.recipe.area.RecipeAreaViewModel
 import com.ivanmorgillo.corsoandroid.teama.settings.SettingsViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -80,5 +79,4 @@ val appModule = module {
     viewModel { HomeViewModel(tracking = get()) }
     viewModel { AreaViewModel(tracking = get(), repository = get()) }
     viewModel { IngredientViewModel(api = get()) }
-
-
+}
